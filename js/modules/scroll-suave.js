0840d167550892dd51fb2343877bc3290a3d4981
Gabriel Kist <gabriel.kist@cwi.com.br>
2023-05-02 10:23:41 -0300

    scroll suave em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,24 +1,36 @@
-export default function initSmoothScroll() {
-  const linksInternos = document.querySelectorAll(
-    "[data-menu='suave'] a[href^='#']"
-  );
+export default class SmoothScroll {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
 
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = this.getAttribute("href");
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-    });
+    if (options === undefined) {
+      this.options = {
+        behavior: "smooth",
+        block: "start",
+      };
+    } else {
+      this.options = options;
+    }
 
-    // forma alternativa
-    // window.scrollTo({
-    //   top: section.offsetTop,
-    //   behavior: "smooth",
-    // });
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
 
-  linksInternos.forEach((link) =>
-    link.addEventListener("click", scrollToSection)
-  );
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) =>
+      link.addEventListener("click", this.scrollToSection)
+    );
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
